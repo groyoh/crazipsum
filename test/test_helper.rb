b165f731ded200752dd630a03bea63c7a6267a92
Yohan Robert <yohan.robert@runtastic.com>
2016-06-23 18:00:33 +0200

    Fix coverall by moving Coveralls.wear on top
    Laboris verilog sunt powerscript dolore vhdl magna anim. Lustre ipsum
    dolore self voluptate incididunt mathematica. Occaecat cillum autolisp
    anim excepteur sed deserunt enim anim. Avenue lorem hypertalk id nulla
    ceylon dolor ad fantom simulink duis fugiat consectetur. Incididunt ml
    dolor labore objective-j id delphi sed consequat et clojure chill
    commodo monkey nostrud. Ut in nimrod velit voluptate magna culpa sunt
    dolor fugiat.
    
    Sint ooc proident rpg ut minim consequat incididunt do excepteur elit
    labore et. F# rexx laboris consectetur velit curl esse magna dolore
    irure. Googleappsscript ad groovy aute veniam elit max/msp pike. Ipsum
    mollit opencl ioke opl duis fugiat exercitation sint anim dolor. Dolor
    sunt sed proident do laboris ocaml excepteur labore dylan simulink.
    
    Magna sed ut dolore voluptate magic mumps aliquip. Nisi viml opencl
    nimrod commodo non informix-4gl in moto exercitation dolor icon
    nulla. Duis lorem proident io minim lpc cfml. Anim eiusmod modula-3 in
    pike laboris fantom j. Quis est mumps nulla elit eiffel clarion. Sqr
    exec culpa pariatur paradox natural sit sqr ut anim cillum id
    aliqua. Clean incididunt cfml ex bc logo velit.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'crazipsum'
-require 'minitest/autorun'
+
 require 'coveralls'
 Coveralls.wear!
+require 'crazipsum'
+require 'minitest/autorun'
